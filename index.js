2d6fb01766dee1151b91b89946c7d0353758cb21
Knilo <azs.schneider@gmail.com>
2025-02-26 15:07:53 +0200

    Fixed config and cached accesory bugs + improved logging

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ module.exports = (api) => {
     // Load the platform logic from the lib folder.
     const PalGatePlatform = require('./lib/palgate.js');
     // Register the platform with Homebridge using the registered alias.
-    api.registerPlatform('PalGateOpener', PalGatePlatform);
+    api.registerPlatform('PalGatePlatform', PalGatePlatform);
   }
